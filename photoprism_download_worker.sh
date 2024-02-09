578525ce4bb707fef74e6ea62b08e336c2fda9a9
Pascal Flöschel <pascal@floeschel.de>
2024-02-09 17:59:58 +0100

    add better rate/threads limiting rename swayimg to pp_client_img.service sync/add config files

--- photoprism_download_worker.sh
+++ photoprism_download_worker.sh
@@ -6,20 +6,23 @@ base_url_dl=$3
 image_dl=$4
 image_date=$5
 
+dl_limit=500K
+thread_limit=2
+
 # Download Images
   echo -e "\nDownloading $count / $length"
   logger -t pp_client "Downloading $count / $length"
 
-  curl -s -S -o /tmp/pp_client-$count $base_url_dl$image_dl
+  curl -s -S --limit-rate $dl_limit -o /tmp/pp_client-$count $base_url_dl$image_dl
 
-  format=$(identify -format '%[compression]' /tmp/pp_client-$count  | tr '[:upper:]' '[:lower:]')
+  format=$(identify -limit thread $thread_limit -format '%[compression]' /tmp/pp_client-$count  | tr '[:upper:]' '[:lower:]')
 
   echo "$count : $format"
   if [[ -z "$format" ]]; then
     format="webp"
-    ffmpeg -i /tmp/pp_client-$count -vcodec libwebp -filter:v fps=fps=5 -loop 0 /tmp/pp_client-$count-resized.$format
+    ffmpeg -threads $thread_limit -i /tmp/pp_client-$count -vcodec libwebp -filter:v fps=fps=5 -loop 0 /tmp/pp_client-$count-resized.$format
   else
-    convert -limit thread 2 -adaptive-resize 1920x1080 -quality 100 /tmp/pp_client-$count /tmp/pp_client-$count-resized.$format
+    convert -limit thread $thread_limit -adaptive-resize 1920x1080 -quality 95 /tmp/pp_client-$count /tmp/pp_client-$count-resized.$format
   fi
   echo -e "Finished conversion $count / $length"
 
