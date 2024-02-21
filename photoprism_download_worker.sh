691f31595d4382dbb341788d17dc14bcc77485f1
Pascal Flöschel <pascal@floeschel.de>
2024-02-21 02:05:42 +0100

    add BingMaps as GEO provider and disable LocationIQ

--- photoprism_download_worker.sh
+++ photoprism_download_worker.sh
@@ -6,10 +6,13 @@ base_url_dl=$3
 image_dl=$4
 image_date=$5
 
-API_KEY=
+
+API_KEY_BING=
+API_KEY_LOCIQ=
 DL_LIMIT=
 THREAD_LIMIT=
 source photoprism_download_worker.env
+
 
 # Download Images
   logger -t pp_client "Starting download + conversion  $count / $length"
@@ -62,27 +65,42 @@ source photoprism_download_worker.env
     exif_lat=$(echo "${exif_lat//+}")
     exif_lon=$(echo "${exif_lon//+}")
 
-    echo "$count : GPS: $exif_lat , $exif_lon"
-    # sleep 1
-    geo_resp=$(curl -s -S --request GET --limit-rate $DL_LIMIT \
-       --url "https://eu1.locationiq.com/v1/reverse?key=$API_KEY&lat=$exif_lat&lon=$exif_lon&normalizeaddress=1&addressdetails=1&format=json&accept-language=en" \
-       --header 'accept: application/json')
+    echo "$count : GPS: $exif_lat,$exif_lon"
+    #GPS --> address
+    #LocationIQ
+    #   geo_resp=$(curl -s -S --request GET --limit-rate $DL_LIMIT \
+    #     --url "https://eu1.locationiq.com/v1/reverse?lat=$exif_lat&lon=$exif_lon&normalizeaddress=1&addressdetails=1&format=json&accept-language=en&key=$API_KEY_LOCIQ" \
+    #     --header 'accept: application/json')
+    #   #echo $geo_resp | jq .
+    #   geo_loc=$(echo "$geo_resp" | jq -r '.address | .road + ", " + .city + ", " + .country')
 
-    #echo $geo_resp | jq .
-    #echo $geo_resp | jq -r '.address | .road + ", " + .city + ", " + .country'
-    geo_loc=$(echo "$geo_resp" | jq -r '.address | .road + ", " + .city + ", " + .country')
+    #BingMaps
+       geo_resp=$(curl -s -S --limit-rate $DL_LIMIT \
+         "http://dev.virtualearth.net/REST/v1/Locations/$exif_lat,$exif_lon?key=$API_KEY_BING" )
+       #echo $geo_resp | jq .
+       geo_loc=$(echo "$geo_resp" | jq -r .resourceSets.[].resources[].name)
+
     geo_loc=$(echo "$geo_loc, $exif_alt")
 
     echo "$count : $geo_loc"
     exiv2 -M "add Exif.Image.Software $geo_loc" $filename
 
     echo "$count : Loading area maps"
-    curl -s -S --limit-rate $DL_LIMIT \
-     "https://maps.locationiq.com/v3/staticmap?key=$API_KEY&size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=11" \
-     -o "images/$image_date--$count.map1.png"
-    curl -s -S --limit-rate $DL_LIMIT \
-     "https://maps.locationiq.com/v3/staticmap?key=$API_KEY&size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=16" \
-     -o "images/$image_date--$count.map2.png"
+    #LocationIQ
+    #  curl -s -S --limit-rate $DL_LIMIT \
+    #    "https://maps.locationiq.com/v3/staticmap?size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=11&key=$API_KEY_LOCIQ" \
+    #    -o "images/$image_date--$count.map1.png"
+    #  curl -s -S --limit-rate $DL_LIMIT \
+    #    "https://maps.locationiq.com/v3/staticmap?size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=16&key=$API_KEY_LOCIQ" \
+    #    -o "images/$image_date--$count.map2.png"
+
+    #BingMaps
+      curl -s -S --limit-rate $DL_LIMIT \
+        "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&pp=$exif_lat,$exif_lon;46&zoomLevel=12&key=$API_KEY_BING" \
+        -o "images/$image_date--$count.map1.jpg"
+      curl -s -S --limit-rate $DL_LIMIT \
+        "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&pp=$exif_lat,$exif_lon;46&zoomLevel=17&key=$API_KEY_BING" \
+        -o "images/$image_date--$count.map2.jpg"
 
    else
     echo "$count : No GPS data found."
