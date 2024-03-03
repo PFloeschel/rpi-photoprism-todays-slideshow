cbb745933c9e2fbac9080b19260cb1b4a66bf66c
Pascal Flöschel <pascal@floeschel.de>
2024-03-03 02:41:00 +0100

    Fix EXIF handling for photo/video, add photo png/avif capability, use PNG for bing maps, whitespace cleanup

--- photoprism_download_worker.sh
+++ photoprism_download_worker.sh
@@ -15,41 +15,44 @@ source photoprism_download_worker.env
 
 
 # Download Images
-  logger -t pp_client "Starting download + conversion  $count / $length"
-  echo -e "\nDownloading $count / $length"
+logger -t pp_client "Starting download + conversion  $count / $length"
+echo -e "\nDownloading $count / $length"
 
-  curl -s -S --limit-rate $DL_LIMIT -o /tmp/pp_client-$count $base_url_dl$image_dl
+curl -s -S --limit-rate $DL_LIMIT -o /tmp/pp_client-$count $base_url_dl$image_dl
 
-  #format=$(identify -limit thread $thread_limit -format '%[compression]' /tmp/pp_client-$count  | tr '[:upper:]' '[:lower:]')
-  format=$(identify -limit thread $THREAD_LIMIT -format '%m\n' /tmp/pp_client-$count  | tr '[:upper:]' '[:lower:]' | head -n1 )
+format=$(identify -limit thread $THREAD_LIMIT -format '%m\n' /tmp/pp_client-$count  | tr '[:upper:]' '[:lower:]' | head -n1 )
 
-  echo "$count : $format"
-  if [[ -z "$format" ]]; then
-    format=$(ffprobe -threads $THREAD_LIMIT -hide_banner -show_format -print_format json /tmp/pp_client-$count | jq -r .format.format_name | cut -d, -f1)
-    format=$(echo "${format//_pipe}")
-    cp -f /tmp/pp_client-$count movies/$image_date--$count.$format
+echo "$count : $format"
 
-    #format="webp"
-    #ffmpeg -hide_banner -threads $THREAD_LIMIT -t 5 -i /tmp/pp_client-$count -vcodec libwebp -r 5 -loop 0 /tmp/pp_client-$count-resized.$format
-    format="avif"
-    ffmpeg -hide_banner -threads $THREAD_LIMIT -t 5 -i /tmp/pp_client-$count -pix_fmt yuv420p -r 5 -f yuv4mpegpipe /tmp/pp_client-$count.y4m
-    avifenc -s 10 -q 35 /tmp/pp_client-$count.y4m /tmp/pp_client-$count-resized.$format -j all
-  else
-    convert -limit thread $THREAD_LIMIT -adaptive-resize 1920x1080 -quality 95 /tmp/pp_client-$count /tmp/pp_client-$count-resized.$format
-  fi
+# VIDEO
+if [[ -z "$format" ]]; then
+  format=$(ffprobe -threads $THREAD_LIMIT -hide_banner -show_format -print_format json /tmp/pp_client-$count | jq -r .format.format_name | cut -d, -f1)
+  format=$(echo "${format//_pipe}")
+  cp -f /tmp/pp_client-$count movies/$image_date--$count.$format
 
-  mv -f /tmp/pp_client-$count-resized.$format images/$image_date--$count.$format
-  rm /tmp/pp_client-$count*
+  #format="webp"
+  #ffmpeg -hide_banner -threads $THREAD_LIMIT -t 5 -i /tmp/pp_client-$count -vcodec libwebp -r 5 -loop 0 /tmp/pp_client-$count-resized.$format
 
+  format="avif"
+  ffmpeg -hide_banner -threads $THREAD_LIMIT -t 10 -i /tmp/pp_client-$count -pix_fmt yuv420p -r 1 -f yuv4mpegpipe /tmp/pp_client-$count.y4m
+  avifenc -p /tmp/pp_client-$count.y4m /tmp/pp_client-$count-resized.$format -j all
 
-# Simplify EXIF for viewing and get GPS location/maps
+# PHOTO
+else
+  # JPEG
+  convert -limit thread $THREAD_LIMIT -adaptive-resize 1920x1080 -quality 100 /tmp/pp_client-$count /tmp/pp_client-$count-resized.$format
 
-  filename="images/$image_date--$count.$format"
+  # PNG / AVIF
+  #format="png"
+  #convert -limit thread $THREAD_LIMIT -adaptive-resize 1920x1080 /tmp/pp_client-$count /tmp/pp_client-$count-resized.$format
+
+  # Simplify EXIF for viewing
+  filename="/tmp/pp_client-$count-resized.$format"
   # simplify camera and exif data
   exif_make=$(exiv2 -Pt -g "Exif.Image.Make" $filename)
   exif_model=$(exiv2 -Pt -g "Exif.Image.Model" $filename)
   exif_cam=$(echo "$exif_make $exif_model")
-  #echo $exif_cam
+
   exiv2 -M "set Exif.Image.Make $exif_cam" $filename
   exiv2 -M 'del Exif.Image.Model' $filename
   exiv2 -M 'del Exif.Image.Software' $filename
@@ -57,13 +60,14 @@ source photoprism_download_worker.env
   exiv2 -M 'del Exif.Photo.FNumber' $filename
   exiv2 -M 'del Exif.Photo.MakerNote' $filename
 
-  # get exif GPS
+  # Get EXIF GPS
   exif_loc=$(exiftool -F -m -location:all -c %+.8f -j $filename)
 
   exif_lat=$(echo "$exif_loc" | jq -r .[].GPSLatitude)
   exif_lon=$(echo "$exif_loc" | jq -r .[].GPSLongitude)
   exif_alt=$(echo "$exif_loc" | jq -r .[].GPSAltitude)
 
+  # GPS --> address
   if [ "$exif_lat" != null ] && [[ -n "$exif_lat" ]] ;
    then
 
@@ -71,54 +75,66 @@ source photoprism_download_worker.env
     exif_lon=$(echo "${exif_lon//+}")
 
     echo "$count : GPS: $exif_lat,$exif_lon"
-    #GPS --> address
-    #LocationIQ
+    # LocationIQ
     #   geo_resp=$(curl -s -S --request GET --limit-rate $DL_LIMIT \
-    #     --url "https://eu1.locationiq.com/v1/reverse?lat=$exif_lat&lon=$exif_lon&normalizeaddress=1&addressdetails=1&format=json&accept-language=en&key=$API_KEY_LOCIQ" \
+    #     --url "https://eu1.locationiq.com/v1/reverse?lat=$exif_lat&lon=$exif_lon&normalizeaddress=1&addressdetails=1&format=json&accept-language=en&key=$>
     #     --header 'accept: application/json')
     #   #echo $geo_resp | jq .
     #   geo_loc=$(echo "$geo_resp" | jq -r '.address | .road + ", " + .city + ", " + .country')
 
-    #BingMaps
-       geo_resp=$(curl -s -S --limit-rate $DL_LIMIT \
-         "http://dev.virtualearth.net/REST/v1/Locations/$exif_lat,$exif_lon?key=$API_KEY_BING" )
-       #echo $geo_resp | jq .
-       geo_loc=$(echo "$geo_resp" | jq -r .resourceSets.[].resources[].name)
-
+    # BingMaps
+    geo_resp=$(curl -s -S --limit-rate $DL_LIMIT \
+       "http://dev.virtualearth.net/REST/v1/Locations/$exif_lat,$exif_lon?key=$API_KEY_BING" )
+    #echo $geo_resp | jq .
+    geo_loc=$(echo "$geo_resp" | jq -r .resourceSets.[].resources[].name)
     geo_loc=$(echo "$geo_loc, $exif_alt")
 
     echo "$count : $geo_loc"
     exiv2 -M "add Exif.Image.Software $geo_loc" $filename
 
-    echo "$count : Loading area maps"
-    #LocationIQ
-    #  curl -s -S --limit-rate $DL_LIMIT \
-    #    "https://maps.locationiq.com/v3/staticmap?size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=11&key=$API_KEY_LOCIQ" \
-    #    -o "images/$image_date--$count.map1.png"
-    #  curl -s -S --limit-rate $DL_LIMIT \
-    #    "https://maps.locationiq.com/v3/staticmap?size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=16&key=$API_KEY_LOCIQ" \
-    #    -o "images/$image_date--$count.map2.png"
-
-    #BingMaps
-      curl -s -S --limit-rate $DL_LIMIT \
-        "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&pp=$exif_lat,$exif_lon;46&zoomLevel=11&key=$API_KEY_BING" \
-        -o "images/$image_date--$count.map1.jpg"
-      curl -s -S --limit-rate $DL_LIMIT \
-        "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&pp=$exif_lat,$exif_lon;46&zoomLevel=15&key=$API_KEY_BING" \
-        -o "images/$image_date--$count.map2.jpg"
-      curl -s -S --limit-rate $DL_LIMIT \
-        "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&pp=$exif_lat,$exif_lon;46&zoomLevel=18&key=$API_KEY_BING" \
-        -o "images/$image_date--$count.map3.jpg"
-
    else
     echo "$count : No GPS data found."
   fi
 
-  # remove files smaller 1024 bytes - cannot be interesting images
-  filesize=$(wc -c $filename | awk '{print $1}')
-  if [ "$filesize" -lt "1024" ]; then
-    rm $filename
-  fi
+  #format="avif"
+  #avifenc -q 95 -p /tmp/pp_client-$count.png /tmp/pp_client-$count-resized.$format -j all
+fi
 
-  echo -e "Finished download + conversion  $count / $length"
-  logger -t pp_client "Finished download + conversion  $count / $length"
+# MOVE from /tmp
+mv -f /tmp/pp_client-$count-resized.$format images/$image_date--$count.$format
+rm /tmp/pp_client-$count*
+
+# Get GPS maps
+if [ "$exif_lat" != null ] && [[ -n "$exif_lat" ]] ;
+ then
+  echo "$count : Loading area maps"
+
+  # LocationIQ
+  #  curl -s -S --limit-rate $DL_LIMIT \
+  #    "https://maps.locationiq.com/v3/staticmap?size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=11&key=$API_KEY_LOCIQ" \
+  #    -o "images/$image_date--$count.map1.png"
+  #  curl -s -S --limit-rate $DL_LIMIT \
+  #    "https://maps.locationiq.com/v3/staticmap?size=960x540&scale=2&markers=icon:small-red-cutout|$exif_lat,$exif_lon&zoom=16&key=$API_KEY_LOCIQ" \
+  #    -o "images/$image_date--$count.map2.png"
+
+  # BingMaps
+    curl -s -S --limit-rate $DL_LIMIT \
+      "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&fmt=png&pp=$exif_lat,$exif_lon;46&zoomLevel=11&key=$API_KEY_BING" \
+      -o "images/$image_date--$count.map1.png"
+    curl -s -S --limit-rate $DL_LIMIT \
+      "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&fmt=png&pp=$exif_lat,$exif_lon;46&zoomLevel=15&key=$API_KEY_BING" \
+      -o "images/$image_date--$count.map2.png"
+    curl -s -S --limit-rate $DL_LIMIT \
+      "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&fmt=png&pp=$exif_lat,$exif_lon;46&zoomLevel=18&key=$API_KEY_BING" \
+      -o "images/$image_date--$count.map3.png"
+fi
+
+# Remove files smaller 1024 bytes - cannot be interesting images
+filename="images/$image_date--$count.$format"
+filesize=$(wc -c $filename | awk '{print $1}')
+if [ "$filesize" -lt "1024" ]; then
+  rm $filename
+fi
+
+echo -e "Finished download + conversion  $count / $length"
+logger -t pp_client "Finished download + conversion  $count / $length"
