f3d4cd009a3c76bb16a67d5ec37129e420918727
Pascal Flöschel <pascal@floeschel.de>
2024-03-12 01:20:34 +0100

    disable map PNGs to HEIC, processing on older RPIs is already slow

--- photoprism_download_worker.sh
+++ photoprism_download_worker.sh
@@ -125,10 +125,10 @@ if [ "$exif_lat" != null ] && [[ -n "$exif_lat" ]] ;
       "https://dev.virtualearth.net/REST/v1/Imagery/Map/AerialWithLabels?mS=1920,1080&dpi=Large&fmt=png&pp=$exif_lat,$exif_lon;46&zoomLevel=18&key=$API_KEY_BING" \
       -o "images/$image_date--$count.map3.png"
 
-  convert -limit thread $THREAD_LIMIT -quality 100 images/$image_date--$count.map1.png images/$image_date--$count.map1.heic
-  convert -limit thread $THREAD_LIMIT -quality 100 images/$image_date--$count.map2.png images/$image_date--$count.map2.heic
-  convert -limit thread $THREAD_LIMIT -quality 100 images/$image_date--$count.map3.png images/$image_date--$count.map3.heic
-  rm images/$image_date--$count.map?.png
+  #convert -limit thread $THREAD_LIMIT -quality 100 images/$image_date--$count.map1.png images/$image_date--$count.map1.heic
+  #convert -limit thread $THREAD_LIMIT -quality 100 images/$image_date--$count.map2.png images/$image_date--$count.map2.heic
+  #convert -limit thread $THREAD_LIMIT -quality 100 images/$image_date--$count.map3.png images/$image_date--$count.map3.heic
+  #rm images/$image_date--$count.map?.png
 fi
 
 # Remove files smaller 1024 bytes - cannot be interesting images
